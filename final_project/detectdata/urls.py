bd59363a57ae3ac21b342b4024261bf1560e7a3a
shpark <offsprings02@naver.com>
2019-11-28 22:31:28 +0900

    update

--- final_project/detectdata/urls.py
+++ final_project/detectdata/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('', views.detectdata.as_view(), name='detectdata'),
 ]
--- final_project/detectdata/views.py
+++ final_project/detectdata/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,HttpResponse,render_to_response
 from .models import Detect
 # Create your views here.
-from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.views import View
 from django.views import generic
 from django.views.generic import TemplateView
 import random, json
@@ -10,31 +11,30 @@ from django.db.models import Q
 
 class detectdata(generic.TemplateView):
     def get(self, request, *args, **kwargs):
-        template_name='detected_list.html'
-        detected_list=Detect.objects.all()
-        return render(request, template_name, {"detected_list" : detected_list})
-
+        template_name='data.html'
+        return render(request, template_name)
+    
 def data_json(request):
-    detected_list=Detect.objects.filter(cid='jh', cname='jh')
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+
+    detected_list=Detect.objects.filter(cid=username, cname=username)
     Times = [None, ]
     Concentrations = ['percent',]
-    Base=[None, ]
     for list in detected_list:
         Times.append(str(list.ctime))
         if list.cpercent>10:
             Concentrations.append(str(list.cpercent))
-            
         else:
             Concentrations.append(0)
-        Base.append(15)
     data = {
         'columns': [
             Times,
             Concentrations,
-            Base,
         ]
     }
     return HttpResponse(json.dumps(data),content_type='text/json')
  
 def main_page(request):
-    return render_to_response('main_page.html')
+    return render_to_response('data.html')
